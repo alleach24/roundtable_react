311b07bd6c36cd3875cb162e2ccb9a7261f7ea11
Sameer <sameer4363@gmail.com>
2023-07-31 22:51:29 -0400

    Add working countdown timer with round options

--- src/screens/RoundTimerScreen.js
+++ src/screens/RoundTimerScreen.js
@@ -1,23 +1,68 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Pressable } from 'react-native';
+
+const roundTimeArr = [1, 3, 4, 6]; // Options for round lengths in minutes
+const recommendedPlayers = ['3 - 5', '6 - 7', '8 - 9', '10+'];
 
 const RoundTimer = ({ navigation }) => {
-  const [isPaused, setIsPaused] = useState(false);
+  const [activeButton, setActiveButton] = useState(2);
+  const [isPaused, setIsPaused] = useState(true);
   const [isComplete, setIsComplete] = useState(false);
   const [milliSecondsRemaining, setMilliSecondsRemaining] = useState(
-    4 * 60 * 100
+    roundTimeArr[activeButton] * 60 * 100
   );
+
+  const handleTimeSelect = (index) => {
+    // Change button styling
+    setActiveButton(index);
+
+    setMilliSecondsRemaining(roundTimeArr[index] * 60 * 100);
+  };
+
+  const handleSkip = () => {
+    setIsPaused(true);
+    setIsComplete(true);
+    setMilliSecondsRemaining(roundTimeArr[activeButton] * 60 * 100);
+  };
 
   return (
     <View style={styles.container}>
+      {isPaused && (
+        <Text>
+          Recommended for {recommendedPlayers[activeButton]} players remaining
+        </Text>
+      )}
+      {isPaused && (
+        <View style={styles.minuteButtonContainer}>
+          {roundTimeArr.map((roundMinutes, index) => (
+            <Pressable
+              onPress={() => handleTimeSelect(index)}
+              style={[
+                styles.button,
+                activeButton === index ? styles.active : null,
+              ]}
+            >
+              <Text style={styles.text}>{roundMinutes} m</Text>
+            </Pressable>
+          ))}
+        </View>
+      )}
+
       <Timer
         time={milliSecondsRemaining}
         setTime={setMilliSecondsRemaining}
         isPaused={isPaused}
         setIsComplete={setIsComplete}
       />
-      <Button title="Start" onPress={() => setIsPaused(false)} />
-      <Button title="Pause" onPress={() => setIsPaused(true)} />
+
+      {isPaused ? (
+        <Button title="Start" onPress={() => setIsPaused(false)} />
+      ) : (
+        <View>
+          <Button title="Pause" onPress={() => setIsPaused(true)} />
+          <Button title="Skip" onPress={handleSkip} />
+        </View>
+      )}
 
       <Button title="Back" onPress={() => navigation.goBack()} />
     </View>
@@ -41,20 +86,19 @@ const Timer = (props) => {
     }
 
     return () => clearInterval(intervalId);
-  }, [isPaused, setTime]);
+  }, [isPaused, setTime, time]);
 
   useEffect(() => {
     if (time <= 0) {
       setIsComplete(true);
-      console.log('timer completed');
     }
-  }, [time]);
+  }, [time, setIsComplete]);
 
   const minutes = Math.floor((time % 360000) / 6000);
   const seconds = Math.floor((time % 6000) / 100);
 
   return (
-    <View style={styles.container}>
+    <View>
       <Text>
         {minutes.toString().padStart(2, '0')}:
         {seconds.toString().padStart(2, '0')}
@@ -73,6 +117,30 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: 'bold',
+  },
+  minuteButtonContainer: {
+    flexDirection: 'row',
+  },
+  active: {
+    backgroundColor: '#42f569',
+    color: '#000000',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    borderWidth: 2,
+    elevation: 3,
+    color: 'black',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: '#000000',
   },
 });
 
